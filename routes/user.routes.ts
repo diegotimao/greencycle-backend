9ce4ebf99b9d6b52ff2dce393b408a9282a97794
Diego <diegotimao1104@gmail.com>
2024-05-23 16:05:50 -0300

    feat: Implement routes create user

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -5,6 +5,7 @@ const router = Router();
 
 const userController = new UserController();
 
-router.get('/users', userController.getAll)
+router.get('/users', userController.getAll);
+router.post('/user/register', userController.create);
 
 export default router;
