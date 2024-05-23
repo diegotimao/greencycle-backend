68bc69c7bffc6746d9d3f8c5a1aa73e979fd3a67
Diego <diegotimao1104@gmail.com>
2024-05-23 16:03:20 -0300

    feat: updated interface IUser

--- interfaces/user.interface.ts
+++ interfaces/user.interface.ts
@@ -1,12 +1,17 @@
-interface User {
+export interface IUser {
   id?: number,
   name: string,
   email: string,
   cpf: string,
   city: string,
   state: string,
-  password_hash: string,
-  avatarUrl?: string
+  password_hash: string
 }
 
-export default User;
+export interface IUserService {
+  create(user: IUser): Promise<IUser | string>
+}
+
+export interface IUserModel {
+  create(user: IUser): Promise<IUser | string>
+}
