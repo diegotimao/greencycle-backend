4f69bda1b1aa76e388d946c52bca331a28aec8db
Diego <diegotimao1104@gmail.com>
2024-05-23 16:03:54 -0300

    feat: Implement metod create in model

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,5 +1,5 @@
 import { Pool, ResultSetHeader } from "mysql2/promise";
-import User from "../interfaces/user.interface";
+import { IUser } from "../interfaces/user.interface";
 
 class UserModel {
   public connection: Pool;
@@ -8,11 +8,42 @@ class UserModel {
     this.connection = connection;
   }
 
-  public async getAll(): Promise<User[]> {
+  public async getAll(): Promise<IUser[]> {
     const result = await this.connection.execute('SELECT * FROM users');
     const [rows] = result;
-    return rows as User[];
+    return rows as IUser[];
   }
-} 
+
+  public async create(user: IUser): Promise<IUser | string> {
+    try {
+      const { name, email, cpf, city, state, password_hash } = user;
+
+      if (await this.userExists('cpf', user.cpf)) {
+        throw new Error('CPF already exists!');
+      }
+
+      if (await this.userExists('email', user.email)) {
+        throw new Error('Email already exists!');
+      }
+
+      const sql = 'INSERT INTO users (name, email, cpf, city, state, password_hash) VALUES (?, ?, ?, ?, ?, ?)';
+      const values = [name, email, cpf, city, state, password_hash];
+      const [result] = await this.connection.execute<ResultSetHeader>(sql, values);
+
+      if (result.affectedRows > 0) {
+        return { id: result.insertId, ...user };
+      }
+
+      throw new Error('Error creating user');
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
+
+  private async userExists(field: string, value: string): Promise<boolean> {
+    const [rows] = await this.connection.execute(`SELECT ${field} FROM users WHERE ${field} = ?`, [value]);
+    return Array.isArray(rows) && rows.length > 0;
+  }
+}
 
 export default UserModel;
