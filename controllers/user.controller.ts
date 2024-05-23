bbce4f401a1ee2bcc03c76f4423b24df8f943d48
Diego <diegotimao1104@gmail.com>
2024-05-23 16:02:32 -0300

    feat: Implement metod create in controller

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -3,11 +3,24 @@ import { StatusCodes } from "http-status-codes";
 import UserService from "../services/user.service";
 
 class UserController {
-  constructor(private userService = new UserService()) {}
+  constructor(private userService = new UserService()) { }
 
   public getAll = async (_req: Request, res: Response) => {
     const users = await this.userService.getAll();
     res.status(StatusCodes.OK).json(users);
+  }
+
+  public create = async (req: Request, res: Response) => {
+    try {
+      const user = req.body;
+      const result = await this.userService.create(user);
+      if (typeof result === 'string') {
+        return res.status(StatusCodes.CONFLICT).json({ message: result });
+      }
+      return res.status(StatusCodes.CREATED).json(result);
+    } catch (error: any) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: error.message })
+    }
   }
 }
 
