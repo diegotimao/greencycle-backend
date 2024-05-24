82c41c3042b36fb08bb9505e9a51f62d687e772b
Diego <diegotimao1104@gmail.com>
2024-05-24 18:12:54 -0300

    feat: Created method updated user in controller

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,37 +1,49 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import UserService from "../services/user.service";
+import { IUserController } from "../interfaces/user.interface";
 
-class UserController {
+class UserController implements IUserController {
   constructor(private userService = new UserService()) { }
 
-  public getAll = async (_req: Request, res: Response) => {
+  public getAll = async (_req: Request, res: Response): Promise<void> => {
     const users = await this.userService.getAll();
     res.status(StatusCodes.OK).json(users);
   }
 
-  public getById = async (req: Request, res: Response) => {
+  public getById = async (req: Request, res: Response): Promise<void> => {
     const id = parseInt(req.params.id);
     console.log(id)
     const user = await this.userService.getById(id);
 
     if (!user) {
-      return res.status(StatusCodes.NOT_FOUND).json({ message: 'User not found!'})
+      res.status(StatusCodes.NOT_FOUND).json({ message: 'User not found!' })
     }
 
     res.status(StatusCodes.OK).json(user)
   }
 
-  public create = async (req: Request, res: Response) => {
+  public create = async (req: Request, res: Response): Promise<void> => {
     try {
       const user = req.body;
       const result = await this.userService.create(user);
       if (typeof result === 'string') {
-        return res.status(StatusCodes.CONFLICT).json({ message: result });
+        res.status(StatusCodes.CONFLICT).json({ message: result });
       }
-      return res.status(StatusCodes.CREATED).json(result);
+      res.status(StatusCodes.CREATED).json(result);
     } catch (error: any) {
-      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: error.message })
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: error.message })
+    }
+  }
+
+  public updated = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const id = Number(req.params.id);
+      const user = req.body;
+      await this.userService.updated(id, user);
+      res.status(StatusCodes.NO_CONTENT).end();
+    } catch (error: any) {
+      res.status(StatusCodes.NOT_FOUND).json(error.message)
     }
   }
 }
