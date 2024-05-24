e1a20a334ba682f5983ce89afbf7a5c4f087f928
Diego <diegotimao1104@gmail.com>
2024-05-24 18:13:40 -0300

    feat: Implement interface class controller

--- interfaces/user.interface.ts
+++ interfaces/user.interface.ts
@@ -1,3 +1,5 @@
+import { Request, Response } from "express";
+
 export interface IUser {
   id?: number,
   name: string,
@@ -8,10 +10,21 @@ export interface IUser {
   password_hash: string
 }
 
+export interface IUserController {
+  getAll: (_req: Request, res: Response) => Promise<void>;
+  getById: (req: Request, res: Response) => Promise<void>;
+  create: (req: Request, res: Response) => Promise<void>;
+  updated: (req: Request, res: Response) => Promise<void>;
+}
+
 export interface IUserService {
+  getAll(): Promise<IUser[]>
+  getById(id: number): Promise<IUser>
   create(user: IUser): Promise<IUser | string>
 }
 
 export interface IUserModel {
+  getAll(): Promise<IUser[]>
+  getById(id: number): Promise<IUser>
   create(user: IUser): Promise<IUser | string>
 }
