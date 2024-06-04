da08e86f09bd0dd580ab4df474cea884d967b4be
Diego <diegotimao1104@gmail.com>
2024-06-04 17:40:08 -0300

    feat: Create class AuthToken

---
+++ utils/authToken.ts
@@ -0,0 +1,35 @@
+import * as jwt from 'jsonwebtoken';
+import { SignOptions } from 'jsonwebtoken';
+import { IUser } from '../interfaces/user.interface';
+
+const SECRET = process.env.JWT_SECRET || 'jwt_secret';
+
+const jwtDefaultConfig: SignOptions = {
+  expiresIn: '1h',
+  algorithm: 'HS256'
+}
+
+export default class AuthToken {
+  constructor(private jwtConfig?: SignOptions) {
+    if (!jwtConfig) {
+      this.jwtConfig = jwtDefaultConfig;
+    }
+  }
+
+  async generateToken(payload: IUser) {
+    return jwt.sign(payload, SECRET, this.jwtConfig);
+  }
+
+  async autenticatorToken(token: string) {
+    try {
+      const autorization: jwt.JwtPayload = jwt.verify(
+        token,
+        SECRET,
+        this.jwtConfig
+      ) as jwt.JwtPayload;
+      return autorization.role;
+    } catch (error) {
+      throw new Error('O token digitado não é valido.')
+    }
+  }
+}
