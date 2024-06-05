19716d76a2d963fa73cc7770d2871b84e21dd145
Diego <diegotimao1104@gmail.com>
2024-06-05 15:51:57 -0300

    feat: Create Interface IEcoponto

---
+++ interfaces/ecoponto.interface.ts
@@ -0,0 +1,14 @@
+
+export interface IEcoponto {
+  id?: number,
+  name: string,
+  email: string,
+  cnpj: string,
+  road: string,
+  houseNumber: number,
+  city: string,
+  neighborhood: string,
+  state: string,
+  phone: string,
+  password: string
+}
