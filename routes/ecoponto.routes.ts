b8264e4d6d6d46a3ea73d1a59915dbeff6744886
Diego <diegotimao1104@gmail.com>
2024-06-06 13:56:02 -0300

    feat: Create routes getById

--- routes/ecoponto.routes.ts
+++ routes/ecoponto.routes.ts
@@ -2,9 +2,9 @@ import { Router } from "express";
 import EcopontoController from "../controllers/ecoponto.controller";
 
 const router = Router();
-
 const ecopontoController = new EcopontoController();
 
 router.get('/ecopontos', ecopontoController.getAll);
+router.get('/ecoponto/:id', ecopontoController.getById);
 
 export default router;
