463a4463a546a49234a589691e8a0f0ca6281c89
Diego <diegotimao1104@gmail.com>
2024-06-08 20:39:59 -0300

    feat: Create routes register and update in routes ecoponto

--- routes/ecoponto.routes.ts
+++ routes/ecoponto.routes.ts
@@ -7,5 +7,7 @@ const ecopontoController = new EcopontoController();
 router.get('/ecopontos', ecopontoController.getAll);
 router.get('/ecoponto/:id', ecopontoController.getById);
 router.delete('/ecoponto/:id', ecopontoController.remove);
+router.post('/ecoponto/register', ecopontoController.create);
+router.post('/ecoponto/:id', ecopontoController.update);
 
 export default router;
