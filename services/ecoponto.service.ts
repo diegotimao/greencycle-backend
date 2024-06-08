a88cee9c346a063256b31c55c00506590c965f08
Diego <diegotimao1104@gmail.com>
2024-06-08 20:40:37 -0300

    feat: Create method update in ecopontoService

--- services/ecoponto.service.ts
+++ services/ecoponto.service.ts
@@ -1,6 +1,9 @@
 import { IEcoponto } from "../interfaces/ecoponto.interface";
 import connection from "../models/connection";
 import EcopontoModel from "../models/ecoponto.model";
+import AuthToken from "../utils/authToken";
+import BcryptService from "../utils/bcryptService";
+import { Ecoponto } from "../validations/Ecoponto";
 
 export default class EcopontoService {
   public model: EcopontoModel;
@@ -35,6 +38,29 @@ export default class EcopontoService {
       };
       await this.model.remove(id);
       return true;
+    } catch (error: any) {
+      throw new Error(error.message);
+    };
+  };
+
+  public async create(ecoponto: IEcoponto): Promise<string> {
+    try {
+      const { password_hash, name, cnpj, email, city, houseNumber, neighborhood, phone, road, state } = ecoponto;
+      console.log(password_hash)
+      const newPasswordHash = await new BcryptService().passwordHash(password_hash);
+      const newEcoponto = { password_hash: newPasswordHash, name, cnpj, email, city, houseNumber, neighborhood, phone, road, state };
+      const resultCreateEcoponto = await this.model.create(newEcoponto);
+      const token = await new AuthToken().generateToken(resultCreateEcoponto as IEcoponto);
+      return token;
+    } catch (error: any) {
+      throw new Error(error.message);
+    };
+  };
+
+  public async update(id: number, ecoponto: IEcoponto): Promise<IEcoponto> {
+    try {
+      const ecpontoUpdeted = await this.model.update(ecoponto); 
+      return ecpontoUpdeted as IEcoponto;
     } catch (error: any) {
       throw new Error(error.message);
     };
