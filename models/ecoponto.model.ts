85dff05616b6d4379c4745ec0c1a1d03b8c07c4c
Diego <diegotimao1104@gmail.com>
2024-06-08 20:38:58 -0300

    feat: Create methods create, update and ecopontoExistis in ecopontoModel

--- models/ecoponto.model.ts
+++ models/ecoponto.model.ts
@@ -47,4 +47,55 @@ export default class EcopontoModel {
       throw new Error(error.message);
     };
   };
-};
+
+  public async create(ecoponto: IEcoponto): Promise<IEcoponto> {
+    try {
+      const { name, cnpj, city, email, houseNumber, neighborhood, password_hash, phone, road, state } = ecoponto;
+      
+      if (await this.ecopontoExists('cnpj', cnpj)) {
+        throw new Error('Já existe uma conta cadastrada com este CNPJ.');
+      };
+
+      if (await this.ecopontoExists('email', email)) {
+        throw new Error('Já existe uma conta cadastrada com este E-mail.')
+      }
+
+      const sql = 'INSERT INTO ecoponto (name, cnpj, city, email, houseNumber, neighborhood, password_hash, phone, road, state) VALUES (?,?,?,?,?,?,?,?,?,?)'
+      const values = [name, cnpj, city, email, houseNumber, neighborhood, password_hash, phone, road, state];
+      const [result] = await this.connection.execute<ResultSetHeader>(sql, values);
+      
+      if (result.affectedRows > 0) {
+        return { id: result.insertId, ...ecoponto };
+      };
+
+      throw new Error('Erro ao cadastrar ecoponto, por favor tente novamente.')
+    } catch (error: any) {
+      throw new Error(error.message);
+    };
+  };
+
+  public async update(ecoponto: IEcoponto): Promise<IEcoponto | undefined> {
+    try {
+      const { name, cnpj, email, city, houseNumber, neighborhood, phone, road, state, id } = ecoponto;
+      const sql = 'UPDATE ecoponto SET name=?, cnpj=?, road=?, houseNumber=?, city=?, neighborhood=?, state=?, phone=?, email=? WHERE id=?';
+      const values = [name, cnpj, road, houseNumber, city, neighborhood, state, phone, email, id];
+      const [resultUpdate]: any = await this.connection.execute(sql, values);
+      const affectedRows = resultUpdate.affectedRows;
+      if (affectedRows > 0) {
+        return ecoponto;
+      }
+    } catch (error: any) {
+      throw new Error(`Error updating ecoponto: ${error.message}`);
+    }
+  }
+  
+
+  public async ecopontoExists(field: string, value: string) {
+    try {
+      const [rows] = await this.connection.execute(`SELECT ${field} FROM ecoponto WHERE ${field} = ?`, [value]);
+      return Array.isArray(rows) && rows.length > 0;
+    } catch (error) {
+      throw new Error('Não foi possivel concluir a busca.')
+    }
+  }
+}; 
