5b4da571952a0fd64cc3c09157ae1e63e4959e73
Diego <diegotimao1104@gmail.com>
2024-06-08 20:35:49 -0300

    refactor: Refactoring user schema validations

---
+++ validations/User.ts
@@ -0,0 +1,38 @@
+import { cpf } from 'cpf-cnpj-validator';
+import { z, ZodError } from 'zod';
+
+const userSchema = z.object({
+  name: z.string(),
+  email: z.string().email({ message: 'E-mail inválido'}),
+  cpf: z.string().refine(cpf.isValid, { message: 'Você deve inserir um cpf válido'}),
+  city: z.string(),
+  state: z.string(),
+  password_hash: z.string().min(6, { message: 'A senha deve ter no minímo 6 caracteres.'}),
+});
+
+type UserData = z.infer<typeof userSchema>;
+
+export class User {
+  name: string;
+  email: string;
+  cpf: string;
+  city: string;
+  state: string;
+  password_hash: string;
+
+  constructor(userData: UserData) {
+    this.name = userData.name;
+    this.email = userData.email;
+    this.cpf = userData.cpf;
+    this.city = userData.city;
+    this.state = userData.state;
+    this.password_hash = userData.password_hash;
+  };
+
+  static validate(userData: unknown): User {
+    const parseData = userSchema.parse(userData);
+    return new User(parseData);
+  }
+}
+
+export { userSchema, ZodError };
