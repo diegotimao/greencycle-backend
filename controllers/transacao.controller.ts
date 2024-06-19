d997e3af5477152b3ceb261ed6ba1fc78c2ea145
Diego <diegotimao1104@gmail.com>
2024-06-19 16:45:49 -0300

    feat: Create method create transacao controller

--- controllers/transacao.controller.ts
+++ controllers/transacao.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
+import { z } from "zod";
 import TransacaoService from "../services/transacao.service";
 import { StatusCodes } from "http-status-codes";
+import { Transacao } from "../validations/Transacao";
 
 class TransacaoController {
   constructor(private transacaoService = new TransacaoService()) { };
@@ -8,10 +10,15 @@ class TransacaoController {
   public create = async (req: Request, res: Response): Promise<void> => {
     try {
       const transacao = req.body;
+      Transacao.validate(transacao);
       const transacaoResponse = await this.transacaoService.create(transacao);
       res.status(StatusCodes.OK).json(transacaoResponse);
     } catch (error: any) {
-      res.status(StatusCodes.NOT_FOUND).json({"error": error.message})
+      if (error instanceof z.ZodError) {
+        res.status(StatusCodes.CONFLICT).json({ message: error.errors[0].message});
+      } else {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
+      };
     };
   };
 };
