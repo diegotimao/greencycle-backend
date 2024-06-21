d0f560b9f6927f069f7dbf701c473a93259e7d87
Diego <diegotimao1104@gmail.com>
2024-06-21 18:25:58 -0300

    feat: Create method getPointsQuilosUser in userModel

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,4 +1,4 @@
-import { Pool, ResultSetHeader } from "mysql2/promise";
+import { FieldPacket, Pool, ResultSetHeader, RowDataPacket } from "mysql2/promise";
 import { IUser } from "../interfaces/user.interface";
 
 class UserModel {
@@ -103,7 +103,25 @@ class UserModel {
   private async userExists(field: string, value: string): Promise<boolean> {
     const [rows] = await this.connection.execute(`SELECT ${field} FROM users WHERE ${field} = ?`, [value]);
     return Array.isArray(rows) && rows.length > 0;
-  }
-}
+  };
+
+  public async getPointsAndQuilosUser(idUser: number): Promise<unknown> {
+    try {
+      const [pointsRows]: [RowDataPacket[], FieldPacket[]] = await this.connection.execute('SELECT total_points AS Pontos FROM account WHERE id_user=?', [idUser]);
+      const points = pointsRows[0]?.Pontos || 0;
+      
+      const [quilosRows]: [RowDataPacket[], FieldPacket[]] = await this.connection.execute(
+        'SELECT FORMAT(SUM(item_transacao.quilogramas), 0) AS Kg FROM item_transacao INNER JOIN transacao ON item_transacao.id = transacao.id WHERE transacao.id_user=?',
+        [idUser]
+      );
+
+      const quilos = quilosRows[0]?.Kg || 0;
+
+      return { points, quilos };
+    } catch (error: any) {
+      throw new Error(error.message)  
+    };
+  };
+};
 
 export default UserModel;
