019e5e243d15e5443364abea14a85b618e65aa19
Diego <diegotimao1104@gmail.com>
2024-06-21 17:00:50 -0300

    feat: Create routes getAllTransacaoUser

--- routes/transacao.routes.ts
+++ routes/transacao.routes.ts
@@ -2,10 +2,10 @@ import { Router } from "express";
 import TransacaoController from "../controllers/transacao.controller";
 
 const router = Router();
-
 const transacaoController = new TransacaoController();
 
 router.post('/transacao', transacaoController.create);
+router.get('/transacao/:id', transacaoController.getAllTransacaoUser);
 router.post('/transacao/cofirmation', transacaoController.confirmationTransacao);
 
 export default router;
