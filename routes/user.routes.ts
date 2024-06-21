16e618d80dc9f1623d07591c38818c38c4a066e7
Diego <diegotimao1104@gmail.com>
2024-06-21 18:26:35 -0300

    feat: Create routes getPointsAndQuilosUser

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -3,13 +3,14 @@ import AuttenticateToken from "../middleware/auntenticateToken";
 import UserController from "../controllers/user.controller";
 
 const router = Router();
-
 const userController = new UserController();
+
+router.post('/users/register', userController.create);
+router.post('/user/login', userController.login);
 
 router.get('/users', userController.getAll);
 router.get('/user/:id', userController.getById);
-router.post('/users/register', userController.create);
-router.post('/user/login', userController.login);
+router.get('/user/points/:id', userController.getPointsAndQuilosUser);
 router.post('/user/:id', AuttenticateToken, userController.updated);
 router.delete('/user/:id', AuttenticateToken, userController.remove);
 
